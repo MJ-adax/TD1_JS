c31d37c542e5464fc1e8bb2dd4ae794da1f3a222
Mathieu Gueribout <mgueribout@gmail.com>
2018-10-16 20:15:26 +0200

    Début tableau

--- Pricesright.js
+++ Pricesright.js
@@ -1,21 +1,36 @@
+var nbuser;
 var dé;
     function nb_aleatoire(max){
-        return dé = Math.floor(Math.random() * max) + 1;
+        var max = 100;
+        dé = Math.floor(Math.random() * max) + 1;
     }
-console.log(nb_aleatoire(100));
-
 
     function jeu(){
+        var essais = 0;
         var nbuser = document.getElementById('nombreuser').value;
         switch(nbuser){
             case (nbuser < dé ? nbuser : null) :
                 document.getElementById('textarea').innerHTML="C'est plus";
+                essais = essais++;
+                console.log(essais);
             break;
             case (nbuser > dé ? nbuser : null) :
                 document.getElementById('textarea').innerHTML="C'est moins";
+                essais = essais++;
+                console.log(essais);
             break;
             case (nbuser == dé ? nbuser : null) :
                 document.getElementById('textarea').innerHTML="C'est GAGNE !";
+                document.getElementsByClassName('td1').innerHTML=dé;
             break;
         }
+        while (nbuser != dé){
+            essais++;
+            console.log(essais);
+        }
+        else {
+            essais = 
+        }
     }
+    
+
